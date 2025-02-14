9a6f0be88a33dd31f80a7a5f0017b23f307ddd89
Khairul Alam <khairul.noman@gmail.com>
2025-02-14 18:44:24 -0500

    fixed error

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,11 @@
 import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
-import Clients from "@/components/Clients";
+// import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
-import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
+// import RecentProjects from "@/components/RecentProjects";
+// import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
   return (
